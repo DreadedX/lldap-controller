28fe0e333d1a9a13ccebf772d72b26ec380219be
Dreaded_X <tim@huizinga.dev>
2025-03-15 05:50:47 +0100

    Adjusted queries
    Since this is becoming a kubernetes controller there is no need to keep
    track of which users are managed in lldap

--- queries/src/lib.rs
+++ queries/src/lib.rs
@@ -3,49 +3,7 @@ pub(crate) mod schema {}
 
 #[derive(cynic::QueryFragment, Debug)]
 #[cynic(graphql_type = "Query")]
-pub struct GetUserAttributes {
-    pub schema: Schema,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-pub struct Schema {
-    pub user_schema: AttributeList,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-pub struct AttributeList {
-    pub attributes: Vec<AttributeSchema>,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-pub struct AttributeSchema {
-    pub name: String,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-#[cynic(graphql_type = "Mutation")]
-pub struct CreateManagedUserAttribute {
-    #[arguments(attributeType: "INTEGER", isEditable: false, isList: false, isVisible: false, name: "managed")]
-    pub add_user_attribute: Success,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-pub struct Success {
-    pub ok: bool,
-}
-
-#[derive(cynic::Enum, Clone, Copy, Debug)]
-pub enum AttributeType {
-    String,
-    Integer,
-    JpegPhoto,
-    DateTime,
-}
-
-#[derive(cynic::QueryFragment, Debug)]
-#[cynic(graphql_type = "Query")]
-pub struct ListManagedUsers {
-    #[arguments(filters: { eq: { field: "managed", value: "1" } })]
+pub struct ListUsers {
     pub users: Vec<User>,
 }
 
@@ -66,6 +24,11 @@ pub struct DeleteUser {
     pub delete_user: Success,
 }
 
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Success {
+    pub ok: bool,
+}
+
 #[derive(cynic::QueryVariables, Debug)]
 pub struct CreateUserVariables<'a> {
     pub id: &'a str,
@@ -74,7 +37,7 @@ pub struct CreateUserVariables<'a> {
 #[derive(cynic::QueryFragment, Debug)]
 #[cynic(graphql_type = "Mutation", variables = "CreateUserVariables")]
 pub struct CreateUser {
-    #[arguments(user: { attributes: { name: "managed", value: "1" }, email: $id, id: $id })]
+    #[arguments(user: { email: $id, id: $id })]
     pub create_user: User,
 }
 
@@ -98,22 +61,8 @@ mod tests {
     use cynic::QueryBuilder;
 
     #[test]
-    fn get_user_attributes_gql_output() {
-        let operation = GetUserAttributes::build(());
-
-        insta::assert_snapshot!(operation.query);
-    }
-
-    #[test]
-    fn create_managed_user_attribute_gql_output() {
-        let operation = CreateManagedUserAttribute::build(());
-
-        insta::assert_snapshot!(operation.query);
-    }
-
-    #[test]
-    fn list_managed_users_gql_output() {
-        let operation = ListManagedUsers::build(());
+    fn list_users_gql_output() {
+        let operation = ListUsers::build(());
 
         insta::assert_snapshot!(operation.query);
     }
